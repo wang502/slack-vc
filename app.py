1ae401bbebbed6097c42ceffee2051ea852524ee
Xiaohui Wang <wang502@indiana.edu>
2016-05-05 17:06:34 -0400

    initial commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello():
     #print portfolio
     #return Response(str(portfolio), content_type='text/plain;charset=utf-8')
     return "hello world"
-    
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=port)
