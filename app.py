be3d408787034f0d51f91eb3f81b1475f924d3d6
Xiaohui Wang <wang502@indiana.edu>
2016-05-05 16:59:21 -0400

    initial commit

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    portfolio = vc.getKhoslaPortfolio()
-    print portfolio
-    return Response(str(portfolio), content_type='text/plain;charset=utf-8')
-
+    #portfolio = vc.getKhoslaPortfolio()
+    #print portfolio
+    #return Response(str(portfolio), content_type='text/plain;charset=utf-8')
+    return "hello world"
+    
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(port=port)
