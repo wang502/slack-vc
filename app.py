697b5b790008b370dd7b5aa249fe46cf853bf8c3
Xiaohui Wang <wang502@indiana.edu>
2016-05-05 16:57:27 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response, redirect
 import vc
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def hello():
     return Response(str(portfolio), content_type='text/plain;charset=utf-8')
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
